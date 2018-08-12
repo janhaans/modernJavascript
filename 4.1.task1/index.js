cb70b3295dbf58cbf9fa7e0ffa7b51c3e6f4f21c
Jan Haans <jan.haans@gmail.com>
2018-08-12 12:56:12 -0700

    Added 4.1task1

---
+++ 4.1.task1/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+console.log(user);
+
+user.name = "Pete";
+console.log(user);
+
+delete user.name;
+console.log(user);
